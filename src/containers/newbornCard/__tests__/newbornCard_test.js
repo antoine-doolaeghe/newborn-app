bd85d5c7b7e6f10632efd561de07695d32879280
Antoine Doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-04-04 08:46:23 +0100

    NEW-24: added basic test for the newborn card

---
+++ src/containers/newbornCard/__tests__/newbornCard_test.js
@@ -0,0 +1,7 @@
+import { fireEvent, cleanup, render } from "react-testing-library";
+
+describe("<NewbornCard />", () => {
+  it("should render a card", () => {
+    expect(true).toBeTruthy();
+  });
+});
