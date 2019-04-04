e4cb527ca4a5add0cfe39af885194afebb778762
Antoine Doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-04-04 09:09:16 +0100

    NEW-24: added tests for newborn card name

--- src/containers/newbornCard/__tests__/newbornCard_test.js
+++ src/containers/newbornCard/__tests__/newbornCard_test.js
@@ -1,7 +1,15 @@
+import React from "react";
 import { fireEvent, cleanup, render } from "react-testing-library";
+import NewbornCard from "../newbornCard";
 
 describe("<NewbornCard />", () => {
+  afterEach(cleanup);
   it("should render a card", () => {
-    expect(true).toBeTruthy();
+    window.HTMLCanvasElement.prototype.getContext = () => {};
+
+    const { getByText } = render(
+      <NewbornCard newbornName="newbornName" newbornSummaries={{}} />
+    );
+    getByText("newbornName");
   });
 });
--- src/containers/newbornCard/newbornCard.js
+++ src/containers/newbornCard/newbornCard.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import { NewbornCard } from './newbornCard.style';
-import CardHeader from '@material-ui/core/CardHeader';
-import Avatar from '@material-ui/core/Avatar';
-import { Line } from 'react-chartjs-2';
-import { Link } from 'react-router-dom';
-import lineChartOptions from './lineChartOptions';
+import React from "react";
+import { withRouter, BrowserRouter } from "react-router-dom";
+import { NewbornCard } from "./newbornCard.style";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
+import { Line } from "react-chartjs-2";
+import { Link } from "react-router-dom";
+import lineChartOptions from "./lineChartOptions";
 
 function NewBornCard(props) {
-  const { newbornId } = props;
+  const { newbornId, newbornName, newbornPlace, newbornSummaries } = props;
   return (
-    <NewbornCard onClick={() => {props.handleNewbornSelect(props.newbornId)}} isSelected={props.isSelected}>
-      <Link to={`./newborn-record/${props.newbornId}`}>
-        <CardHeader avatar={<Avatar aria-label="Recipe" />} title={props.newbornName} subheader={props.newbornPlace} />
-      </Link>    
+    <NewbornCard
+      onClick={() => {
+        props.handleNewbornSelect(props.newbornId);
+      }}
+      isSelected={props.isSelected}
+    >
+      <BrowserRouter>
+        <Link to={`./newborn-record/${props.newbornId}`}>
+          <CardHeader
+            avatar={<Avatar aria-label="Recipe" />}
+            title={newbornName}
+            subheader={newbornPlace}
+          />
+        </Link>
+      </BrowserRouter>
       <Line
         options={lineChartOptions}
-        data={props.newbornSummaries}
+        data={newbornSummaries}
         data-newborn-id={newbornId}
         height={192}
       />
