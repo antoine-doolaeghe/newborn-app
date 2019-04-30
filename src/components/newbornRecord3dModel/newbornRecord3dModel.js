4c220add198882b084851f89387957684cd64659
Antoine Doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-04-30 19:50:40 +0100

    NEW-54: refactored 3d model component to split the three.js functionality

--- src/components/newbornRecord3dModel/newbornRecord3dModel.js
+++ src/components/newbornRecord3dModel/newbornRecord3dModel.js
@@ -5,60 +5,16 @@ import { NewbornRecord3dModelContainer } from "./newbornRecord3dModel.style";
 
 class NewBornRecord3dModel extends Component {
   componentDidUpdate() {
-    if (this.props.newbornModelInfo) {
-      const width = this.mount.clientWidth;
-      const height = this.mount.clientHeight;
-      // ADD SCENE
-      this.scene = new THREE.Scene();
-      // ADD CAMERA
-      this.camera = new THREE.PerspectiveCamera(15, width / height, 1, 10000);
-      this.controls = new OrbitControls(this.camera);
-      // ADD CUBE
-      const geometry = new THREE.SphereGeometry(1, 32, 32);
-      const material = new THREE.MeshPhongMaterial({
-        color: 0x6c6c6c
-      });
-      this.props.newbornModelInfo.cellPositions.forEach(position => {
-        const child = new THREE.Mesh(geometry, material);
-        child.position.set(position[0], position[1], position[2]);
-        child.receiveShadow = true;
-        this.controls.target = child.position;
-        this.scene.add(child);
-      });
-      this.controls.autoRotate = true;
-      this.controls.autoZoom = true;
-      this.controls.update();
-      const light = new THREE.DirectionalLight(0xdfebff, 1.75);
-      light.position.set(300, 400, 50);
-      light.position.multiplyScalar(1.3);
-
-      light.castShadow = true;
-      light.shadowCameraVisible = true;
-
-      light.shadowMapWidth = 512;
-      light.shadowMapHeight = 512;
-
-      const d = 200;
-
-      light.shadowCameraLeft = -d;
-      light.shadowCameraRight = d;
-      light.shadowCameraTop = d;
-      light.shadowCameraBottom = -d;
-
-      light.shadowCameraFar = 1000;
-      light.shadowDarkness = 0.2;
-
-      this.scene.add(light);
-
-      this.scene.background = new THREE.Color(0xaaaaaa);
-
-      this.renderer = new THREE.WebGLRenderer();
-      this.renderer.setSize(width, height);
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.renderer.shadowMap.enabled = true;
-      this.renderer.gammaInput = true;
-      this.renderer.gammaOutput = true;
-      this.mount.appendChild(this.renderer.domElement);
+    const { newbornModelInfo } = this.props;
+    if (newbornModelInfo) {
+      this.width = this.mount.clientWidth;
+      this.height = this.mount.clientHeight;
+      this.buildScene();
+      this.buildCamera();
+      this.setOrbitalControls();
+      this.buildNewbornCells();
+      this.buildSceneLight();
+      this.setRenderer();
       this.start();
     }
   }
@@ -68,6 +24,76 @@ class NewBornRecord3dModel extends Component {
       this.mount.removeChild(this.renderer.domElement);
     }
   }
+
+  buildScene = () => {
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xaaaaaa);
+  };
+
+  buildCamera = () => {
+    this.camera = new THREE.PerspectiveCamera(
+      15,
+      this.width / this.height,
+      1,
+      10000
+    );
+  };
+
+  setOrbitalControls = () => {
+    this.controls = new OrbitControls(this.camera);
+    this.controls.autoRotate = true;
+    this.controls.autoZoom = true;
+    this.controls.update();
+  };
+
+  buildNewbornCells = () => {
+    const { newbornModelInfo } = this.props;
+    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    const material = new THREE.MeshPhongMaterial({
+      color: 0x6c6c6c
+    });
+    newbornModelInfo.cellPositions.forEach(position => {
+      const child = new THREE.Mesh(geometry, material);
+      child.position.set(position[0], position[1], position[2]);
+      child.receiveShadow = true;
+      this.controls.target = child.position;
+      this.scene.add(child);
+    });
+  };
+
+  buildSceneLight = () => {
+    const light = new THREE.DirectionalLight(0xdfebff, 1.75);
+    light.position.set(300, 400, 50);
+    light.position.multiplyScalar(1.3);
+
+    light.castShadow = true;
+    light.shadowCameraVisible = true;
+
+    light.shadowMapWidth = 512;
+    light.shadowMapHeight = 512;
+
+    const d = 200;
+
+    light.shadowCameraLeft = -d;
+    light.shadowCameraRight = d;
+    light.shadowCameraTop = d;
+    light.shadowCameraBottom = -d;
+
+    light.shadowCameraFar = 1000;
+    light.shadowDarkness = 0.2;
+
+    this.scene.add(light);
+  };
+
+  setRenderer = () => {
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.gammaInput = true;
+    this.renderer.gammaOutput = true;
+    this.mount.appendChild(this.renderer.domElement);
+  };
 
   start = () => {
     if (!this.frameId) {
