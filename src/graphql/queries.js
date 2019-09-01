6aa29475bd1b415c9dba03e370643a73ee500a8e
Antoine Doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-09-01 20:37:12 +0100

    NEW-144: no summary data placeholder message

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -78,6 +78,7 @@ export const listGenerations = `query ListGenerations(
         items {
           bio
           bornPlace
+          birthDate
           childs
           createdAt
           hexColor
