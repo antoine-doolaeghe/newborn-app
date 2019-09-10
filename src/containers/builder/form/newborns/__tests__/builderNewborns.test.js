1d97cc43c577eedb7bb1b97c14281614ffec6df6
Antoine Doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-09-10 21:37:57 +0100

    NEW-230: added test for builderNewborn

--- src/containers/builder/form/newborns/__tests__/builderNewborns.test.js
+++ src/containers/builder/form/newborns/__tests__/builderNewborns.test.js
@@ -1,11 +1,52 @@
 import React from "react";
 import { render } from "react-testing-library";
-import BuilderNewborns from "../builderNewborns";
+import { MockedProvider } from "react-apollo/test-utils";
+import gql from "graphql-tag";
+import BuilderNewborn from "../builderNewborns";
+import { updateNewborn } from "../../../../../graphql/mutations";
 
-describe("Badge Component", () => {
-  it("should render", () => {
-    const { debug } = render(<BuilderNewborns label="label" />);
-    // debug();
-    // expect(getByText("label")).toBeTruthy();
+const API_MOCKS = [
+  {
+    request: {
+      query: gql(updateNewborn),
+      variables: {
+        name: "test-id"
+      }
+    },
+    result: {
+      data: {
+        dog: { id: "1", name: "Buck", breed: "bulldog" }
+      }
+    }
+  }
+];
+
+const MOCK_NEWBORNS = [
+  {
+    name: "newborn1",
+    id: "1"
+  },
+  {
+    name: "newborn2",
+    id: "2"
+  }
+];
+
+const props = {
+  userNewborns: MOCK_NEWBORNS,
+  trainerNewborns: [],
+  refetch: jest.fn(),
+  trainerId: "mockId"
+};
+
+describe("Builder Newborn List Component", () => {
+  it("should the list of newborns", () => {
+    const { getAllByTestId } = render(
+      <MockedProvider mocks={API_MOCKS}>
+        <BuilderNewborn {...props} />
+      </MockedProvider>
+    );
+    getAllByTestId("newborn_select_1_id");
+    getAllByTestId("newborn_select_1_id");
   });
 });
